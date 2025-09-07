d0af87e22cd2b727311a0830a60028595f540563
Gangaprasad Urekar <gangaprasadcode63@gmail.com>
2025-09-07 19:59:20 +0530

    100

--- .cph/Graph6.java
+++ .cph/Graph6.java
@@ -101,19 +101,103 @@
 
 // /////// Bridge in Graph(Tarjans Algorithm) \\ \\\\\\
 
+// import java.util.*;
+// public class Graph6 {
+//     static class  Edge {
+//         int src;
+//         int dest;
+//         public Edge(int src,int dest){
+//             this.src=src;
+//             this.dest=dest;
+//         }
+//     }
+//     public static void createGraph(ArrayList<Edge> graph[]){
+//         for(int i=0;i<graph.length;i++){
+//             graph[i]=new ArrayList<Edge>();
+//         }
+
+//         graph[0].add(new Edge(0, 1));
+//         graph[0].add(new Edge(0, 2));
+//         graph[0].add(new Edge(0, 3));
+
+//         graph[1].add(new Edge(1, 0));
+//         graph[1].add(new Edge(1, 2));
+
+//         graph[2].add(new Edge(2, 0));
+//         graph[2].add(new Edge(2, 1));
+
+//         graph[3].add(new Edge(3, 0));
+//         graph[3].add(new Edge(3, 4));
+//         graph[3].add(new Edge(3, 5));
+
+//         graph[4].add(new Edge(4, 3));
+//         graph[4].add(new Edge(4, 5));
+
+//         graph[5].add(new Edge(5, 3));
+//         graph[5].add(new Edge(5, 4));
+//     }
+//     public static void dfs(ArrayList<Edge> graph[],int curr,int parent,int dt[],int low[],int time, boolean visited[]){
+//        visited[curr] = true;
+//        dt[curr]=low[curr]=++time;
+
+//        for(int i=0;i<graph[curr].size();i++){
+//         Edge e = graph[curr].get(i);
+//         int neighbor = e.dest;
+//         if(neighbor == parent){
+//             continue;
+//         }else if(!visited[neighbor]){
+//             dfs(graph, neighbor, curr, dt, low, time, visited);
+//             low[curr]=Math.min(low[curr],low[neighbor]);
+//             if(dt[curr]<low[neighbor]){
+//                 System.out.println("Bridge : "+curr+"--->"+neighbor);
+//             }
+//         }else{
+//             low[curr]=Math.min(low[curr],dt[neighbor]);
+//         }
+//        }
+
+//     }
+
+//     public static void TarjansBridge(ArrayList<Edge> graph[],int v){
+//         int dt[]=new int[v];  // discovery time
+//         int low[]=new int[v];  // lowest discovery time
+//         int time=0;
+//         boolean visited[]=new boolean[v];
+
+//         for(int i=0;i<v;i++){
+//             if(!visited[i]){
+//                 dfs(graph,i,-1,dt,low,time,visited);
+//             }
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int v=6;
+//         ArrayList<Edge> graph[]=new ArrayList[v];
+//         createGraph(graph);
+//         TarjansBridge(graph,v);
+//     }
+// }
+
+
+
+
+
+// //////// Articulation Point(Tarjans Algorithm)
+
 import java.util.*;
 public class Graph6 {
-    static class  Edge {
-        int src;
-        int dest;
-        public Edge(int src,int dest){
-            this.src=src;
-            this.dest=dest;
+    static class Edge {
+        int src, dest;
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
         }
     }
-    public static void createGraph(ArrayList<Edge> graph[]){
-        for(int i=0;i<graph.length;i++){
-            graph[i]=new ArrayList<Edge>();
+
+    // Create Graph
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
         }
 
         graph[0].add(new Edge(0, 1));
@@ -128,52 +212,74 @@ public class Graph6 {
 
         graph[3].add(new Edge(3, 0));
         graph[3].add(new Edge(3, 4));
-        graph[3].add(new Edge(3, 5));
 
         graph[4].add(new Edge(4, 3));
         graph[4].add(new Edge(4, 5));
 
-        graph[5].add(new Edge(5, 3));
         graph[5].add(new Edge(5, 4));
     }
-    public static void dfs(ArrayList<Edge> graph[],int curr,int parent,int dt[],int low[],int time, boolean visited[]){
-       visited[curr] = true;
-       dt[curr]=low[curr]=++time;
 
-       for(int i=0;i<graph[curr].size();i++){
-        Edge e = graph[curr].get(i);
-        int neighbor = e.dest;
-        if(neighbor == parent){
-            continue;
-        }else if(!visited[neighbor]){
-            dfs(graph, neighbor, curr, dt, low, time, visited);
-            low[curr]=Math.min(low[curr],low[neighbor]);
-            if(dt[curr]<low[neighbor]){
-                System.out.println("Bridge : "+curr+"--->"+neighbor);
-            }
-        }else{
-            low[curr]=Math.min(low[curr],dt[neighbor]);
-        }
-       }
+    // Tarjan’s Algorithm for Articulation Points
+    public static void dfs(ArrayList<Edge>[] graph, int curr, int parent, boolean[] vis,
+                           int[] dt, int[] low, int time, boolean[] ap) {
+        vis[curr] = true;
+        dt[curr] = low[curr] = ++time;   // initial dt and low 
+        int children = 0;
 
-    }
+        for (int i=0;i<graph[curr].size();i++) {
+            Edge e = graph[curr].get(i);
+            int neigh = e.dest;
 
-    public static void TarjansBridge(ArrayList<Edge> graph[],int v){
-        int dt[]=new int[v];  // discovery time
-        int low[]=new int[v];  // lowest discovery time
-        int time=0;
-        boolean visited[]=new boolean[v];
+            // case 1:
+            if (neigh == parent) continue;
 
-        for(int i=0;i<v;i++){
-            if(!visited[i]){
-                dfs(graph,i,-1,dt,low,time,visited);
+            // case 2:
+            if (!vis[neigh]) {
+                dfs(graph, neigh, curr, vis, dt, low, time, ap);
+                low[curr] = Math.min(low[curr], low[neigh]);
+
+                // Check articulation point condition
+                if (parent != -1 && dt[curr] <= low[neigh]) {
+                    ap[curr] = true;
+                }
+                children++;
+            } else {
+                // Back edge
+                low[curr] = Math.min(low[curr], dt[neigh]);
             }
         }
+
+        // Special case for root
+        if (parent == -1 && children > 1) {
+            ap[curr] = true;
+        }
     }
+
+    public static void getAP(ArrayList<Edge>[] graph, int V) {
+        int[] dt = new int[V];    // discovery time 
+        int[] low = new int[V];   // lowest discovery time
+        boolean[] vis = new boolean[V];
+        boolean[] ap = new boolean[V];
+
+        int time = 0;
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(graph, i, -1, vis, dt, low, time, ap);
+            }
+        }
+
+        System.out.println("Articulation Points are: ");
+        for (int i = 0; i < V; i++) {
+            if (ap[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int v=6;
-        ArrayList<Edge> graph[]=new ArrayList[v];
+        int V = 6;
+        ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph);
-        TarjansBridge(graph,v);
+        getAP(graph, V);
     }
 }
